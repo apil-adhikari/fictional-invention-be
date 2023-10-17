3cca6f89cd55a03e9b259cf97b81152e07df4c87
Apil Adhikari <apiladhikari207@gmail.com>
2023-10-17 09:54:26 +0545

    Moved from Callback Hell to Promises

--- Asynchronous JS/index.js
+++ Asynchronous JS/index.js
@@ -3,15 +3,16 @@ const superagent = require('superagent');
 
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed: ${data}`);
+
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      //Error handling: if ${data} doesn't exist, return error->message and end. Don't go further.
-      if (err) return console.log(err.message);
+    .then((res) => {
       console.log(res.body.message);
 
-      fs.writeFile('dog-img.txt', res.body.message, (err) =>
-        console.log('Random dog image saved to file.')
-      );
-    });
+      fs.writeFile('dog-img.txt', res.body.message, (err) => {
+        if (err) return console.log(err.message);
+        console.log('Random dog image saved to file!');
+      });
+    })
+    .catch((err) => console.log(err.message));
 });
