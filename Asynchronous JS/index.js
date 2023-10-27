e5e81fa25ac57cf51125c4c4c78eef9a8de912e0
Apil Adhikari <apiladhikari207@gmail.com>
2023-10-27 17:16:55 +0545

    Add Async/Await using Promises with try-catch block

--- Asynchronous JS/index.js
+++ Asynchronous JS/index.js
@@ -19,16 +19,37 @@ const writeFilePro = (file, data) => {
   });
 };
 
-readFilePro(`${__dirname}/dogg.txt`)
-  .then((data) => {
-    console.log(`Breed: ${data}`);
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-  .then((res) => {
+// Async / Await with try-catch block
+
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(`Breed : ${data}`);
+
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
     console.log(res.body.message);
-    return writeFilePro('dog-img.txt', res.body.message);
-  })
-  .then(() => {
+
+    await writeFilePro('dog-img.txt', res.body.message);
     console.log('Random dog image saved to file!');
-  })
-  .catch((err) => console.log(err));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getDogPic();
+
+// readFilePro(`${__dirname}/dogg.txt`)
+//   .then((data) => {
+//     console.log(`Breed: ${data}`);
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then((res) => {
+//     console.log(res.body.message);
+//     return writeFilePro('dog-img.txt', res.body.message);
+//   })
+//   .then(() => {
+//     console.log('Random dog image saved to file!');
+//   })
+//   .catch((err) => console.log(err));
