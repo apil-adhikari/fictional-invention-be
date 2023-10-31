3243b84e07c7e110ffa8f7eb2ced3f2b14e85cb6
Apil Adhikari <apiladhikari207@gmail.com>
2023-10-31 12:40:37 +0545

    Handled POST Requests

--- Natours/app.js
+++ Natours/app.js
@@ -2,6 +2,10 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
+
+// Middleware
+app.use(express.json());
+
 const port = 3000;
 
 // app.get('/', (req, res) => {
@@ -19,6 +23,7 @@ const tours = JSON.parse(
 );
 
 app.get('/api/v1/tours', (req, res) => {
+  // using JSEND specification for the response
   res.status(200).json({
     status: 'success',
     result: tours.length,
@@ -26,6 +31,28 @@ app.get('/api/v1/tours', (req, res) => {
       tours: tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/datat/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 // Listining on port
