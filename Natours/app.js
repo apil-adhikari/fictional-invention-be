5b73fec3402caae88e386108b4478c5e6c09b735
Apil Adhikari <apiladhikari207@gmail.com>
2023-10-31 11:22:59 +0545

    Started API handling GET Requests

--- Natours/app.js
+++ Natours/app.js
@@ -1,16 +1,31 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', App: 'Natours' });
-});
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', App: 'Natours' });
+// });
 
-app.post('/', (req, res) => {
-  res.status(200).send('You can now post in this URL...');
+// app.post('/', (req, res) => {
+//   res.status(200).send('You can now post in this URL...');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
 });
 
 // Listining on port
