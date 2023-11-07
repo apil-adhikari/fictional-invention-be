458aac14b9cc5b270c9636e1341e1bf309acc5e4
Apil Adhikari <apiladhikari207@gmail.com>
2023-11-07 22:14:16 +0545

    Created a better file structure for Routes & Controllers

--- Natours/app.js
+++ Natours/app.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
 
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
 const app = express();
 
 // 1) MIDDLEWARES
@@ -21,8 +24,6 @@ app.use((req, res, next) => {
   next();
 });
 
-const port = 3000;
-
 // app.get('/', (req, res) => {
 //   res
 //     .status(200)
@@ -35,151 +36,7 @@ const port = 3000;
 
 //JSON. parse() is a crucial method for converting JSON data in string form into Javascript objects.
 // typeof tours->object;after parsing
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
 
-/*------------------------*/
-// 2) REQUEST HANDLERS
-const getAllTours = (req, res) => {
-  // using JSEND specification for the response
-  /*
-  {
-    status : "success",
-    // Enveloping
-    data : {
-      ---Original Object--
-      
-    }
-  }
-  */
-
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    result: tours.length,
-    data: {
-      // url-endpoint(eg:/tours):actual-object,
-      tours: tours,
-    },
-  });
-};
-
-const getTour = (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1;
-
-  // finding the the spcecific object that has id on the request parameter.
-  // .find() method returns true if the condition is met otherwise undefined
-  const tour = tours.find((el) => el.id === id);
-
-  // checking if the requested id exists?
-  if (id > tours.length) {
-    // if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'ID not found',
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tours: tour,
-    },
-  });
-};
-
-const createTour = (req, res) => {
-  // console.log(req.body);
-
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/datat/tours-simple.json`,
-    JSON.stringify(tours), //converting JS values to JSON
-    (err) => {
-      res.status(201).json({
-        //.status(201)-The request has been fulfilled and resulted in a new resource being created
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-};
-
-const updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated Tour here...>',
-    },
-  });
-};
-
-const deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-};
-
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
-
-const getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
-
-const createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
-
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
-
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
-
-/*------------------------*/
 // .get():retrive the data
 // app.get('/api/v1/tours', getAllTours);
 
@@ -209,21 +66,11 @@ const deleteUser = (req, res) => {
 
 // 3) ROUTES
 
-// Mounting Router
-const tourRouter = express.Router();
-const userRouter = express.Router();
+// Mounting Router(multiple routers)
+// Created Routers
 
-tourRouter.route('/').get(getAllTours).post(createTour);
-tourRouter.route('/id').get(getTour).patch(createTour).delete(deleteTour);
-
-userRouter.route('/').get(getAllUsers).post(createUser);
-userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
-
+// Mounting Routers
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-// Listining on port
-// 4) START SERVER
-app.listen(port, '127.0.0.1', () => {
-  console.log(`App is running on port ${port}...`);
-});
+module.exports = app;
---
+++ Natours/controllers/tourController.js
@@ -0,0 +1,108 @@
+const fs = require('fs');
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+exports.getAllTours = (req, res) => {
+  // using JSEND specification for the response
+  /*
+      {
+        status : "success",
+        // Enveloping
+        data : {
+          ---Original Object--
+          
+        }
+      }
+      */
+
+  console.log(req.requestTime);
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    result: tours.length,
+    data: {
+      // url-endpoint(eg:/tours):actual-object,
+      tours: tours,
+    },
+  });
+};
+
+exports.getTour = (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+
+  // finding the the spcecific object that has id on the request parameter.
+  // .find() method returns true if the condition is met otherwise undefined
+  const tour = tours.find((el) => el.id === id);
+
+  // checking if the requested id exists?
+  if (id > tours.length) {
+    // if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'ID not found',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours: tour,
+    },
+  });
+};
+
+exports.createTour = (req, res) => {
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/datat/tours-simple.json`,
+    JSON.stringify(tours), //converting JS values to JSON
+    (err) => {
+      res.status(201).json({
+        //.status(201)-The request has been fulfilled and resulted in a new resource being created
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+
+exports.updateTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated Tour here...>',
+    },
+  });
+};
+
+exports.deleteTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
