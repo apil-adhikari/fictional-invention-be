e3416d391afef7e1e70a19bad06d7eaefee8e344
Apil Adhikari <apiladhikari207@gmail.com>
2023-11-13 12:53:06 +0545

    Added the files for database connection

--- Natours/app.js
+++ Natours/app.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const morgan = require('morgan');
+
 const express = require('express');
+
 const app = express();
 
 const tourRouter = require('./routes/tourRoutes');
--- Natours/server.js
+++ Natours/server.js
@@ -1,13 +1,54 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-
 const app = require('./app');
+
+dotenv.config({ path: './config.env' });
 // env set by Express
 // console.log(app.get('env'));
 
 // env set by Node
 // console.log(process.env);
 // console.log(process.env);
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    // useNewUrlParser: true,
+    // useCreatseIndex: true,
+    // useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log('DB connection successful!');
+    console.log(con.connection);
+  });
+
+// Schema for tours
+// describing schema:(Blueprint of logical layout of database)
+const tourSchema = new mongoose.Schema({
+  // field : data-type,
+  // Schema Type Options
+
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+});
+
+// modle
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, '127.0.0.1', () => {
