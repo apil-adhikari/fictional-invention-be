32856df76356a81c535a59a027cdf156878eab57
Apil Adhikari <apiladhikari207@gmail.com>
2023-11-19 16:10:35 +0545

    Updated TOUR Model

--- Natours/models/tourModel.js
+++ Natours/models/tourModel.js
@@ -10,15 +10,52 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: [true, 'A tour name must be unique'],
+    trim: true,
+  },
+  duration: {
+    type: Number,
+    require: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A group must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    require: [true, 'A group must have diffuculty'],
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
-  rating: {
+  ratingsAverage: {
     type: Number,
     default: 4.5,
   },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 // modle
